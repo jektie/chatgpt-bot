b2325e9990fbd027b6f34baad246b5f48f4c0118
jektie <jektie@jekties-MacBook-Air.local>
2025-07-23 20:47:23 +0700

    แก้ไขโค้ด error code400

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/line', async (req, res) => {
         'https://api.line.me/v2/bot/message/reply',
         {
           replyToken,
-          messages
+          messages: [{ type: 'text', text: reply }]
         },
         {
           headers: {
